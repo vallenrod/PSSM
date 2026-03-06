7408c754ea0281494f389ec9cb2470f5d2f43de5
vallenrod <15969996+vallenrod@users.noreply.github.com>
2026-03-06 11:55:26 -0500

    Update PSSM.ps1

--- PSSM.ps1
+++ PSSM.ps1
@@ -85,6 +85,7 @@ function List-Startup {
     $i = 1
     $Global:StartupEntries | ForEach-Object {
         Write-Host ("[{0}] {1,-20} | {2,-18} `n {3}" -f $i, $_.Type, $_.Name, $_.Command)
+        Start-Sleep -Milliseconds 20
         $i++
     }
     Write-Host "`nTotal: $($Global:StartupEntries.Count)`n"
@@ -101,6 +102,7 @@ function Disable-RegistryItem {
     $i = 1
     $regItems | ForEach-Object {
         Write-Host ("[{0}] {1,-18} | {2,-20} `n {3}" -f $i, $_.Type, $_.Name, $_.Command)
+        Start-Sleep -Milliseconds 20
         $i++
     }
 
@@ -127,6 +129,7 @@ function Reenable-RegistryItem {
     $i = 1
     $disabledItems | ForEach-Object {
         Write-Host ("[{0}] {1,-18} | {2,-25} `n {3}" -f $i, $_.Type, $_.Name, $_.Command)
+        Start-Sleep -Milliseconds 20
         $i++
     }
 
